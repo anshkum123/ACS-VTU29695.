f268192c5fc0e4d97308b8270801743532256684
anshkum123 <vtu29695@veltech.edu.in>
2026-09-10 09:46:19 +0530

    Add files via upload

---
+++ Task 39 Binary Tree Inorder Traversal.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+
+            // Go to the leftmost node
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            // Get the node
+            current = stack.pop();
+            result.add(current.val);
+
+            // Move to right
+            current = current.right;
+        }
+
+        return result;
+    }
+}
